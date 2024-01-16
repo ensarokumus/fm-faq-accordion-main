28b9be75729acc3bd4ab7fff5ec5c7a9f0eae794
ensaro <41113027+ensarokumus@users.noreply.github.com>
2024-01-16 23:10:48 +1100

    Add ThemeProvider to change default font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { Typography } from "@mui/material"
+import { Typography, createTheme, ThemeProvider } from "@mui/material";
 
 function App() {
-  return <Typography>Hello World!</Typography>;
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        "Work Sans",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography>Hello World!</Typography>
+    </ThemeProvider>
+  );
 }
 
 export default App;
